2392cdee523406f07d258cd68a00fcf037e3d1e2
Raul-guii <raulawp460@gmail.com>
2026-02-13 05:38:29 -0300

    first commit

---
+++ backend/sgf/src/test/java/sgf/example/sgf/SgfApplicationTests.java
@@ -0,0 +1,13 @@
+package sgf.example.sgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
